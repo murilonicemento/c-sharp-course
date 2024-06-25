d97f74d38ad0d1fdf0b54ab74dbc9fd0d6cb239e
murilonicemento <murilo_nascimento22@outlook.com>
2024-06-24 22:30:39 -0300

    namespaces emm c#

---
+++ namespaces/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SampleNamespace
+{
+  class SampleClass
+  {
+    public static void SampleMethod()
+    {
+      System.Console.WriteLine("SampleMethod inside SampleNamespace");
+    }
+  }
+}
+
+namespace AnotherSampleNamespace;
+
+class AnotherSampleClass
+{
+  public static void AnotherSampleMethod()
+  {
+    System.Console.WriteLine("SampleMethod inside SampleNamespace");
+  }
+}
