983388df603e0716ee9468144cc83660cefab65e
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-03 22:59:10 -0300

    add new exercicies

---
+++ exercism/exceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public static class SimpleCalculator
+{
+  public static string Calculate(int operand1, int operand2, string operation)
+  {
+    switch (operation)
+    {
+      case "+":
+        return $"{operand1} + {operand2} = {operand1 + operand2}";
+      case "*":
+        return $"{operand1} * {operand2} = {operand1 * operand2}";
+      case "/":
+        if (operand2 == 0)
+        {
+          return "Division by zero is not allowed.";
+        }
+        return $"{operand1} / {operand2} = {operand1 / operand2}";
+      case "":
+        throw new ArgumentException();
+      case null:
+        throw new ArgumentNullException();
+      default:
+        throw new ArgumentOutOfRangeException();
+    }
+  }
+}
