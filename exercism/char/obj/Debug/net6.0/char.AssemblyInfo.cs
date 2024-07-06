4cad3ce522663236b57f4cff36e45c4b7fb97cb2
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-06 15:42:10 -0300

    add new exercicies properties

--- exercism/char/obj/Debug/net6.0/char.AssemblyInfo.cs
+++ exercism/char/obj/Debug/net6.0/char.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("char")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d4cab8dc02217de3ab29b7d98330ad716e2b34")]
 [assembly: System.Reflection.AssemblyProductAttribute("char")]
 [assembly: System.Reflection.AssemblyTitleAttribute("char")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ exercism/properties/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class WeighingMachine
+{
+  // TODO Define the 'Precision' property
+  private int precision;
+  public int Precision { get { return this.precision; } set { Precision = this.precision; } }
+
+  // TODO Define the 'Weight' property
+  private double weight;
+  public double Weight
+  {
+    get { return this.weight; }
+    set
+    {
+      if (value < 0)
+      {
+        throw new ArgumentOutOfRangeException();
+      }
+
+      this.weight = value;
+    }
+  }
+
+  // TODO Define the 'DisplayWeight' property
+  public string DisplayWeight
+  {
+    get
+    {
+      double displayWeight = Math.Round(Weight - TareAdjustment, Precision);
+      return $"{displayWeight.ToString("f" + Precision)} kg";
+    }
+  }
+
+  // TODO Define the 'TareAdjustment' property
+  public double TareAdjustment { get; set; } = 5;
+
+  public WeighingMachine(int precision)
+  {
+    this.precision = precision;
+  }
+}
