7732553fe4d907ce76c91f03000922c7d293b35a
murilonicemento <murilo_nascimento22@outlook.com>
2024-07-11 22:32:59 -0300

    working with files

---
+++ exercism/files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Course;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    string sourcePath = @"file.txt";
+    string targetPath = @"file2.txt";
+
+    try
+    {
+      FileInfo fileInfo = new FileInfo(sourcePath);
+      fileInfo.CopyTo(targetPath);
+
+      string[] lines = File.ReadAllLines(sourcePath);
+
+      foreach (string line in lines)
+      {
+        Console.WriteLine(line);
+      }
+    }
+    catch (IOException e)
+    {
+      Console.WriteLine("An error ocurred");
+      Console.WriteLine(e.Message);
+    }
+  }
+}
